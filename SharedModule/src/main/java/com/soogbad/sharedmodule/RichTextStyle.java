d8a0bc63e04f848ebf3bea89b11ec1265612a437
Sagi Klein <soogbad@gmail.com>
2026-04-09 19:00:41 +0300

    changed default text color

--- SharedModule/src/main/java/com/soogbad/sharedmodule/RichTextStyle.java
+++ SharedModule/src/main/java/com/soogbad/sharedmodule/RichTextStyle.java
@@ -75,7 +75,7 @@ public class RichTextStyle<T extends CharacterStyle> {
     public static RichTextStyle<ForegroundColorSpan> TEXT_COLOR(TextColor color) { return new RichTextStyle<>(ForegroundColorSpan.class, color.color); }
 
     public static final TextSize DEFAULT_TEXT_SIZE = TextSize.SIZE_16;
-    public static final TextColor DEFAULT_TEXT_COLOR = TextColor.BLACK;
+    public static final TextColor DEFAULT_TEXT_COLOR = TextColor.WHITE;
 
     public enum TextSize {
         SIZE_12(12), SIZE_14(14), SIZE_16(16), SIZE_18(18), SIZE_20(20), SIZE_24(24), SIZE_28(28), SIZE_32(32);
@@ -85,7 +85,7 @@ public class RichTextStyle<T extends CharacterStyle> {
     }
 
     public enum TextColor {
-        BLACK(0xFF000000), RED(0xFFFF0000), GREEN(0xFF00FF00), BLUE(0xFF0000FF), YELLOW(0xFFFFFF00), CYAN(0xFF00FFFF), MAGENTA(0xFFFF00FF);
+        WHITE(0xFFFFFFFF), RED(0xFFFF0000), GREEN(0xFF00FF00), BLUE(0xFF0000FF), YELLOW(0xFFFFFF00), CYAN(0xFF00FFFF), MAGENTA(0xFFFF00FF);
 
         TextColor(int color) { this.color = color; }
         public final int color;
