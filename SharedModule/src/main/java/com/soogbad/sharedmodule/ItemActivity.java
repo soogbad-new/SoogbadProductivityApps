c61778434656c08b0406e6fa42d73cc225c04283
soogbad-new <soogbad@gmail.com>
2026-05-30 20:18:11 +0300

    .

---
+++ SharedModule/src/main/java/com/soogbad/sharedmodule/ItemActivity.java
@@ -0,0 +1,50 @@
+package com.soogbad.sharedmodule;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.WindowInsetsCompat;
+
+public abstract class ItemActivity extends AppCompatActivity {
+
+    protected ItemLayout itemLayout;
+    protected ItemsManager<?, ?> itemsManager;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        itemLayout = findViewById(R.id.itemLayout); ItemActionBar itemActionBar = findViewById(R.id.itemActionBar);
+        itemsManager = ((ItemApplication<?, ?>)getApplication()).getItemsManager();
+        String uuid = getIntent().getStringExtra("item_uuid");
+        Item<?> item = itemsManager.getItem(uuid);
+        if(item == null) {
+            Toast.makeText(this, "Item not found", Toast.LENGTH_SHORT).show();
+            finishAndRemoveTask();
+            return;
+        }
+        itemLayout.init(itemActionBar, itemsManager, item);
+        onItemLoaded(item);
+    }
+
+    protected void onItemLoaded(Item<?> item) {}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        itemLayout.save();
+    }
+
+    public WindowInsetsCompat onApplyWindowInsetsListener(View view, WindowInsetsCompat insets) {
+        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        view.getRootView().findViewById(R.id.toolbar).setPadding(0, systemBars.top, 0, 0);
+        view.setPadding(0, 0, 0, systemBars.bottom);
+        Insets keyboard = insets.getInsets(WindowInsetsCompat.Type.ime());
+        itemLayout.getFormattingToolbar().setPadding(0, 0, 0, keyboard.bottom - systemBars.bottom);
+        return insets;
+    }
+
+}
