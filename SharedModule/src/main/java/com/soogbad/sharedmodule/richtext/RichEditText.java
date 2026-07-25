9defdfe4a20937f85cd443ec760907e71e663c9b
soogbad-new <soogbad@gmail.com>
2026-07-25 17:14:29 +0300

    selection too

--- SharedModule/src/main/java/com/soogbad/sharedmodule/richtext/RichEditText.java
+++ SharedModule/src/main/java/com/soogbad/sharedmodule/richtext/RichEditText.java
@@ -128,12 +128,10 @@ public class RichEditText extends AppCompatEditText {
 
     private static void applyActiveCharacterStyle(Editable editable, int changeStart, int changeCount, RichCharacterStyle<?> style, boolean isActive) {
         if(isActive) {
-            if(style.isDefaultValue())
-                removeSpansInRange(editable, changeStart, changeStart + changeCount, style);
-            else if(!isEntireRangeCovered(editable, changeStart, changeStart + changeCount, style)) {
-                removeSpansInRange(editable, changeStart, changeStart + changeCount, style);
+            boolean wasCovered = isEntireRangeCovered(editable, changeStart, changeStart + changeCount, style);
+            removeSpansInRange(editable, changeStart, changeStart + changeCount, style);
+            if(!wasCovered && !style.isDefaultValue())
                 editable.setSpan(style.createSpan(), changeStart, changeStart + changeCount, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
-            }
         }
         else if(style.isFlagStyle())
             removeSpansInRange(editable, changeStart, changeStart + changeCount, style);
@@ -180,7 +178,7 @@ public class RichEditText extends AppCompatEditText {
     private static void applyStyleToSelection(Editable editable, int selectionStart, int selectionEnd, RichCharacterStyle<?> style) {
         boolean wasCovered = isEntireRangeCovered(editable, selectionStart, selectionEnd, style);
         removeSpansInRange(editable, selectionStart, selectionEnd, style);
-        if(!wasCovered)
+        if(!wasCovered && !style.isDefaultValue())
             editable.setSpan(style.createSpan(), selectionStart, selectionEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
     private void toggleActiveCharacterStyleFlag(RichCharacterStyle<?> style) {
